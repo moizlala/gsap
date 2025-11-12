dce28d16092db7d622664bc930cdd775b3706771
moizlala <hasanabdulmoiz@gmail.com>
2025-11-12 22:21:28 +0500

    second section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -66,7 +66,7 @@ const Hero = () => {
 
   return (
     <>
-    <section id='hero' className=''>
+    <section id='hero' className='noisy'>
         <h1 className='title'> MOJITO </h1>
 
         <img src='/images/hero-left-leaf.png' alt='left-leaf' className='left-leaf'/>
